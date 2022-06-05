a5116f114878d3914b0be90d999d520ca424fd62
Esteban Palma <epp.publico@gmail.com>
2022-06-04 20:29:00 -0600

    function creates static table and return colones amount base on specific exchange rate in table

--- forecast_dataframe.py
+++ forecast_dataframe.py
@@ -2,29 +2,26 @@ from pickle import TRUE
 import pandas as pd
 import numpy as np
 
-# data 
-data = {'startdate':['05/06/2022'],
-        'enddate':['06/04/2022'],
-        'startexch':['670.91'],
-        'endexch':['690.89']
+def getcolones(do):
+    data = {'startdate':['05/06/2022','06/04/2022','07/03/2022','08/01/2022','08/30/2022','09/28/2022','10/27/2022','11/25/2022'],
+        'enddate':['06/04/2022','07/03/2022','08/01/2022','08/30/2022','09/28/2022','10/27/2022','11/25/2022','12/24/2022'],
+        'startexch':['670.91','690.89','711.47','732.65','754.47','776.94','800.08','823.9'],
+        'endexch':['690.89','711.47','732.65','754.47','776.94','800.08','823.9','848.44']
         }
 
-# data = {'startdate':['06/05/2022','04/06/2022','03/07/2022','01/08/2022','30/08/2022','28/09/2022','27/10/2022','25/11/2022'],
-#         'enddate':['04/06/2022','03/07/2022','01/08/2022','30/08/2022','28/09/2022','27/10/2022','25/11/2022','24/12/2022'],
-#         'startexch':[670,91],
-#         'endexch':[690,89]
-#         }
+    # Create dataframe
+    df = pd.DataFrame(data)
+    df['startdate'] = pd.to_datetime(df['startdate'], infer_datetime_format=True)
+    df['enddate'] = pd.to_datetime(df['enddate'], infer_datetime_format=True)
+    df = df.astype({"startexch": float}, errors='raise')
+    df = df.astype({"endexch": float}, errors='raise')
+    # Create calculated fields
+    df['days'] = df['enddate'].sub(df['startdate'], axis=0)
+    df['delta'] = df['endexch'] - df['startexch']
 
-# Create dataframe
+    newcolones12 = do * df['endexch'].iloc[7]
+    newcolones3 = do * df['endexch'].iloc[3]
 
-df = pd.DataFrame(data)
-df['startdate'] = pd.to_datetime(df['startdate'], infer_datetime_format=True)
-df['enddate'] = pd.to_datetime(df['enddate'], infer_datetime_format=True)
-df = df.astype({"startexch": float}, errors='raise')
-df = df.astype({"endexch": float}, errors='raise')
-print(df)
+    return newcolones12, newcolones3
 
-# Create calculated fields
-
-df['days'] = df['enddate'].sub(df['startdate'], axis=0)
-df['delta'] = df['endexch'] - df['startexch']
+getcolones(1500)
